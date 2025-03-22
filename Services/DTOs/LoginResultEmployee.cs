13d19ffc3c7ab41535258776b2249504bd106e75
pankajgharge <pankaj.gharge@gmail.com>
2025-03-22 20:27:07 +0530

    Employ Login API code implementation

---
+++ Services/DTOs/LoginResultEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopDemoProject.Services
+{
+    public class LoginResultEmployee
+    {
+        public string message { get; set; }
+        public string Token { get; set; }
+        public LoginUserAdmin User { get; set; }
+    }
+}
